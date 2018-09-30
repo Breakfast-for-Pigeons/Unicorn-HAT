f53b1cdd178512c35932d01cbc9471ce65218c77
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2018-09-29 19:42:49 -0500

    Update pibow_candy_rainbow_2.py
    Converted to Python 3

--- Pibow_Candy/pibow_candy_rainbow_2.py
+++ Pibow_Candy/pibow_candy_rainbow_2.py
@@ -1,16 +1,35 @@
-#!/usr/bin/env python
-################################################################
-#                 Pibow Candy Rainbow 2                        #
-################################################################
-# Description:                                                 #
-# It's like Pibow Canday Rainbow except that it moves in the   #
-# other direction.                                             #
-#                                                              #
-# Author: Paul Ryan                                            #
-#                                                              #
-################################################################
+#!/usr/bin/python3
+"""
+Pibow Candy Rainbow
 
-import unicornhat, signal, time, random
+This program displays the pastel colors of the Pimoroni Pibow Candy
+case. It's like Pibow Canday Rainbow except that it moves in the other
+direction.
+
+....................
+
+Functions:
+- pibow_candy_rainbow_2: Lights up the LEDs in the pastel colors of the
+  Pimoroni Pibow Candy case
+- stop: Print exit message and turn off the UnicornHAT
+
+....................
+
+Author: Paul Ryan
+
+This program was written on a Raspberry Pi using the Geany IDE.
+"""
+########################################################################
+#                          Import modules                              #
+########################################################################
+
+from time import sleep
+from print_unicornhat_header import print_unicornhat_header
+import unicornhat
+
+########################################################################
+#                           Initialize                                 #
+########################################################################
 
 unicornhat.set_layout(unicornhat.HAT)
 unicornhat.brightness(0.5)
@@ -24,126 +43,154 @@ B = (119, 158, 203)
 I = (150, 111, 214)
 V = (203, 153, 201)
 W = (244, 154, 194)
-off = (0, 0, 0)
 
-def pibow_candy_rainbow_2():
+########################################################################
+#                            Functions                                 #
+########################################################################
 
-	rainbow0 = [
-		[R, O, Y, G, B, I, V, W], 
-		[R, O, Y, G, B, I, V, W], 
-		[R, O, Y, G, B, I, V, W], 
-		[R, O, Y, G, B, I, V, W], 
-		[R, O, Y, G, B, I, V, W], 
-		[R, O, Y, G, B, I, V, W], 
-		[R, O, Y, G, B, I, V, W], 
-		[R, O, Y, G, B, I, V, W]
-	]
-
-	rainbow1 = [
-		[W, R, O, Y, G, B, I, V], 
-		[W, R, O, Y, G, B, I, V], 
-		[W, R, O, Y, G, B, I, V], 
-		[W, R, O, Y, G, B, I, V], 
-		[W, R, O, Y, G, B, I, V], 
-		[W, R, O, Y, G, B, I, V], 
-		[W, R, O, Y, G, B, I, V], 
-		[W, R, O, Y, G, B, I, V]
-	]
-
-	rainbow2 = [
-		[V, W, R, O, Y, G, B, I], 
-		[V, W, R, O, Y, G, B, I], 
-		[V, W, R, O, Y, G, B, I], 
-		[V, W, R, O, Y, G, B, I], 
-		[V, W, R, O, Y, G, B, I], 
-		[V, W, R, O, Y, G, B, I], 
-		[V, W, R, O, Y, G, B, I], 
-		[V, W, R, O, Y, G, B, I]
-	]
-
-	rainbow3 = [
-		[I, V, W, R, O, Y, G, B], 
-		[I, V, W, R, O, Y, G, B], 
-		[I, V, W, R, O, Y, G, B], 
-		[I, V, W, R, O, Y, G, B],
-		[I, V, W, R, O, Y, G, B], 
-		[I, V, W, R, O, Y, G, B], 
-		[I, V, W, R, O, Y, G, B], 
-		[I, V, W, R, O, Y, G, B]
-	]
-
-	rainbow4 = [
-		[B, I, V, W, R, O, Y, G], 
-		[B, I, V, W, R, O, Y, G], 
-		[B, I, V, W, R, O, Y, G], 
-		[B, I, V, W, R, O, Y, G], 
-		[B, I, V, W, R, O, Y, G], 
-		[B, I, V, W, R, O, Y, G], 
-		[B, I, V, W, R, O, Y, G], 
-		[B, I, V, W, R, O, Y, G]
-	]
-
-	rainbow5 = [
-		[G, B, I, V, W, R, O, Y], 
-		[G, B, I, V, W, R, O, Y], 
-		[G, B, I, V, W, R, O, Y], 
-		[G, B, I, V, W, R, O, Y], 
-		[G, B, I, V, W, R, O, Y], 
-		[G, B, I, V, W, R, O, Y], 
-		[G, B, I, V, W, R, O, Y], 
-		[G, B, I, V, W, R, O, Y]
-	]
-
-	rainbow6 = [
-		[Y, G, B, I, V, W, R, O],
-		[Y, G, B, I, V, W, R, O], 
-		[Y, G, B, I, V, W, R, O], 
-		[Y, G, B, I, V, W, R, O], 
-		[Y, G, B, I, V, W, R, O],
-		[Y, G, B, I, V, W, R, O], 
-		[Y, G, B, I, V, W, R, O], 
-		[Y, G, B, I, V, W, R, O]
-	]
-
-	rainbow7 = [
-		[O, Y, G, B, I, V, W, R], 
-		[O, Y, G, B, I, V, W, R], 
-		[O, Y, G, B, I, V, W, R], 
-		[O, Y, G, B, I, V, W, R], 
-		[O, Y, G, B, I, V, W, R], 
-		[O, Y, G, B, I, V, W, R], 
-		[O, Y, G, B, I, V, W, R], 
-		[O, Y, G, B, I, V, W, R] 
-	]
-
-	unicornhat.set_pixels(rainbow0)
-	unicornhat.show()
-	time.sleep(0.5)
-	unicornhat.set_pixels(rainbow1)
-	unicornhat.show()
-	time.sleep(0.5)
-	unicornhat.set_pixels(rainbow2)
-	unicornhat.show()
-	time.sleep(0.5)
-	unicornhat.set_pixels(rainbow3)
-	unicornhat.show()
-	time.sleep(0.5)
-	unicornhat.set_pixels(rainbow4)
-	unicornhat.show()
-	time.sleep(0.5)
-	unicornhat.set_pixels(rainbow5)
-	unicornhat.show()
-	time.sleep(0.5)
-	unicornhat.set_pixels(rainbow6)
-	unicornhat.show()
-	time.sleep(0.5)
-	unicornhat.set_pixels(rainbow7)
-	unicornhat.show()
-	time.sleep(0.5)
 
 def main():
-	while True:
-		pibow_candy_rainbow_2()
+    """
+    This is the main function
+    """
+    
+    print_unicornhat_header()
+    
+    # Force white text after selecting random colored header
+    print("\033[1;37;40mPress Ctrl-C to stop the program.")
+    
+    try:
+        while True:
+            pibow_candy_rainbow_2()
+    except KeyboardInterrupt:
+        stop()
+
+
+def pibow_candy_rainbow_2():
+    """
+    Lights up the LEDs in the pastel colors of the Pimoroni
+    Pibow Candy case
+    """
+
+    rainbow0 = [
+        [R, O, Y, G, B, I, V, W],
+        [R, O, Y, G, B, I, V, W],
+        [R, O, Y, G, B, I, V, W],
+        [R, O, Y, G, B, I, V, W],
+        [R, O, Y, G, B, I, V, W],
+        [R, O, Y, G, B, I, V, W],
+        [R, O, Y, G, B, I, V, W],
+        [R, O, Y, G, B, I, V, W]
+    ]
+
+    rainbow1 = [
+        [W, R, O, Y, G, B, I, V],
+        [W, R, O, Y, G, B, I, V],
+        [W, R, O, Y, G, B, I, V],
+        [W, R, O, Y, G, B, I, V],
+        [W, R, O, Y, G, B, I, V],
+        [W, R, O, Y, G, B, I, V],
+        [W, R, O, Y, G, B, I, V],
+        [W, R, O, Y, G, B, I, V]
+    ]
+
+    rainbow2 = [
+        [V, W, R, O, Y, G, B, I],
+        [V, W, R, O, Y, G, B, I],
+        [V, W, R, O, Y, G, B, I],
+        [V, W, R, O, Y, G, B, I],
+        [V, W, R, O, Y, G, B, I],
+        [V, W, R, O, Y, G, B, I],
+        [V, W, R, O, Y, G, B, I],
+        [V, W, R, O, Y, G, B, I]
+    ]
+
+    rainbow3 = [
+        [I, V, W, R, O, Y, G, B],
+        [I, V, W, R, O, Y, G, B],
+        [I, V, W, R, O, Y, G, B],
+        [I, V, W, R, O, Y, G, B],
+        [I, V, W, R, O, Y, G, B],
+        [I, V, W, R, O, Y, G, B],
+        [I, V, W, R, O, Y, G, B],
+        [I, V, W, R, O, Y, G, B]
+    ]
+
+    rainbow4 = [
+        [B, I, V, W, R, O, Y, G],
+        [B, I, V, W, R, O, Y, G],
+        [B, I, V, W, R, O, Y, G],
+        [B, I, V, W, R, O, Y, G],
+        [B, I, V, W, R, O, Y, G],
+        [B, I, V, W, R, O, Y, G],
+        [B, I, V, W, R, O, Y, G],
+        [B, I, V, W, R, O, Y, G]
+    ]
+
+    rainbow5 = [
+        [G, B, I, V, W, R, O, Y],
+        [G, B, I, V, W, R, O, Y],
+        [G, B, I, V, W, R, O, Y],
+        [G, B, I, V, W, R, O, Y],
+        [G, B, I, V, W, R, O, Y],
+        [G, B, I, V, W, R, O, Y],
+        [G, B, I, V, W, R, O, Y],
+        [G, B, I, V, W, R, O, Y]
+    ]
+
+    rainbow6 = [
+        [Y, G, B, I, V, W, R, O],
+        [Y, G, B, I, V, W, R, O],
+        [Y, G, B, I, V, W, R, O],
+        [Y, G, B, I, V, W, R, O],
+        [Y, G, B, I, V, W, R, O],
+        [Y, G, B, I, V, W, R, O],
+        [Y, G, B, I, V, W, R, O],
+        [Y, G, B, I, V, W, R, O]
+    ]
+
+    rainbow7 = [
+        [O, Y, G, B, I, V, W, R],
+        [O, Y, G, B, I, V, W, R],
+        [O, Y, G, B, I, V, W, R],
+        [O, Y, G, B, I, V, W, R],
+        [O, Y, G, B, I, V, W, R],
+        [O, Y, G, B, I, V, W, R],
+        [O, Y, G, B, I, V, W, R],
+        [O, Y, G, B, I, V, W, R]
+    ]
+
+    unicornhat.set_pixels(rainbow0)
+    unicornhat.show()
+    sleep(0.5)
+    unicornhat.set_pixels(rainbow1)
+    unicornhat.show()
+    sleep(0.5)
+    unicornhat.set_pixels(rainbow2)
+    unicornhat.show()
+    sleep(0.5)
+    unicornhat.set_pixels(rainbow3)
+    unicornhat.show()
+    sleep(0.5)
+    unicornhat.set_pixels(rainbow4)
+    unicornhat.show()
+    sleep(0.5)
+    unicornhat.set_pixels(rainbow5)
+    unicornhat.show()
+    sleep(0.5)
+    unicornhat.set_pixels(rainbow6)
+    unicornhat.show()
+    sleep(0.5)
+    unicornhat.set_pixels(rainbow7)
+    unicornhat.show()
+    sleep(0.5)
+
+def stop():
+    """
+    Print exit message and turn off the UnicornHAT
+    """
+    print("\nExiting program.")
+    unicornhat.off()
 
 if __name__ == '__main__':
-	main()
+    main()
