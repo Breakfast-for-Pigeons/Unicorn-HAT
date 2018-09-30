ccb428a0227dcf64ca283ab06d20d62a5bad3ec6
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2018-09-29 23:08:37 -0500

    Update pibow_candy_double_ripple_4.py
    Converted to Python 3

--- Pibow_Candy/pibow_candy_double_ripple_4.py
+++ Pibow_Candy/pibow_candy_double_ripple_4.py
@@ -1,15 +1,36 @@
-#!/usr/bin/env python
-################################################################
-#                Pibow Candy Double Ripple 4                   #
-################################################################
-# Description:                                                 #
-# This program "ripples" the Pibow Candy rainbow.              #
-#                                                              #
-# Author: Paul Ryan                                            #
-#                                                              #
-################################################################
+#!/usr/bin/python3
+"""
+Pibow Candy Double Ripple 4
 
-import unicornhat, signal, time, random
+This program displays and the pastel colors of the Pimoroni Pibow
+Candy case. It ripples the Pibow Candy rainbow from the bottom left
+(next to the USB ports) to the top right (The corner near the power
+port) as well as from the bottom to the top (the side with the HDMI
+port).
+
+....................
+
+Functions:
+- pibow_candy_double_ripple_4: Ripples the Pibow Candy rainbow
+- stop: Print exit message and turn off the UnicornHAT
+
+....................
+
+Author: Paul Ryan
+
+This program was written on a Raspberry Pi using the Geany IDE.
+"""
+########################################################################
+#                          Import modules                              #
+########################################################################
+
+from time import sleep
+import unicornhat
+from print_unicornhat_header import print_unicornhat_header
+
+########################################################################
+#                           Initialize                                 #
+########################################################################
 
 unicornhat.set_layout(unicornhat.HAT)
 unicornhat.brightness(0.5)
@@ -32,139 +53,171 @@ I2 = (75, 55, 107)
 V2 = (101, 76, 100)
 W2 = (122, 77, 97)
 
-def pibow_candy_double_ripple_4():
+########################################################################
+#                            Functions                                 #
+########################################################################
 
-	rainbow = [
-		[R, O, Y, G, B, I, V, W], 
-		[R, O, Y, G, B, I, V, W], 
-		[R, O, Y, G, B, I, V, W], 
-		[R, O, Y, G, B, I, V, W], 
-		[R, O, Y, G, B, I, V, W], 
-		[R, O, Y, G, B, I, V, W], 
-		[R, O, Y, G, B, I, V, W], 
-		[R, O, Y, G, B, I, V, W]
-	]
-
-	ripple00 = [
-		[R2, O, Y, G, B, I, V, W], 
-		[R2, O, Y, G, B, I, V, W], 
-		[R2, O, Y, G, B, I, V, W], 
-		[R2, O, Y, G, B, I, V, W], 
-		[R2, O, Y, G, B, I, V, W], 
-		[R2, O, Y, G, B, I, V, W], 
-		[R2, O, Y, G, B, I, V, W], 
-		[R2, O2, Y2, G2, B2, I2, V2, W2]
-]
-
-	ripple01 = [
-		[R, O2, Y, G, B, I, V, W], 
-		[R, O2, Y, G, B, I, V, W], 
-		[R, O2, Y, G, B, I, V, W], 
-		[R, O2, Y, G, B, I, V, W], 
-		[R, O2, Y, G, B, I, V, W], 
-		[R, O2, Y, G, B, I, V, W], 
-		[R2, O2, Y2, G2, B2, I2, V2, W2], 
-		[R, O2, Y, G, B, I, V, W]
-	]
-
-	ripple02 = [
-		[R, O, Y2, G, B, I, V, W], 
-		[R, O, Y2, G, B, I, V, W], 
-		[R, O, Y2, G, B, I, V, W], 
-		[R, O, Y2, G, B, I, V, W], 
-		[R, O, Y2, G, B, I, V, W], 
-		[R2, O2, Y2, G2, B2, I2, V2, W2], 
-		[R, O, Y2, G, B, I, V, W], 
-		[R, O, Y2, G, B, I, V, W]
-	]
-
-	ripple03 = [
-		[R, O, Y, G2, B, I, V, W], 
-		[R, O, Y, G2, B, I, V, W], 
-		[R, O, Y, G2, B, I, V, W], 
-		[R, O, Y, G2, B, I, V, W], 
-		[R2, O2, Y2, G2, B2, I2, V2, W2], 
-		[R, O, Y, G2, B, I, V, W], 
-		[R, O, Y, G2, B, I, V, W], 
-		[R, O, Y, G2, B, I, V, W]
-	]
-
-	ripple04 = [
-		[R, O, Y, G, B2, I, V, W], 
-		[R, O, Y, G, B2, I, V, W], 
-		[R, O, Y, G, B2, I, V, W], 
-		[R2, O2, Y2, G2, B2, I2, V2, W2], 
-		[R, O, Y, G, B2, I, V, W], 
-		[R, O, Y, G, B2, I, V, W], 
-		[R, O, Y, G, B2, I, V, W], 
-		[R, O, Y, G, B2, I, V, W]
-	]
-
-	ripple05 = [
-		[R, O, Y, G, B, I2, V, W], 
-		[R, O, Y, G, B, I2, V, W], 
-		[R, O, Y, G, B, I2, V, W], 
-		[R2, O2, Y2, G2, B2, I2, V2, W2], 
-		[R, O, Y, G, B, I2, V, W], 
-		[R, O, Y, G, B, I2, V, W], 
-		[R, O, Y, G, B, I2, V, W], 
-		[R, O, Y, G, B, I2, V, W]
-	]
-
-	ripple06 = [
-		[R, O, Y, G, B, I, V, W], 
-		[R2, O2, Y2, G2, B2, I2, V2, W2], 
-		[R, O, Y, G, B, I, V, W], 
-		[R, O, Y, G, B, I, V, W], 
-		[R, O, Y, G, B, I, V, W], 
-		[R, O, Y, G, B, I, V, W], 
-		[R, O, Y, G, B, I, V, W], 
-		[R, O, Y, G, B, I, V, W]
-	]
-
-	ripple07 = [
-		[R2, O2, Y2, G2, B2, I2, V2, W2], 
-		[R, O, Y, G, B, I, V, W2], 
-		[R, O, Y, G, B, I, V, W2], 
-		[R, O, Y, G, B, I, V, W2], 
-		[R, O, Y, G, B, I, V, W2], 
-		[R, O, Y, G, B, I, V, W2], 
-		[R, O, Y, G, B, I, V, W2], 
-		[R, O, Y, G, B, I, V, W2]
-	]
-
-	while True:
-		unicornhat.set_pixels(rainbow)
-		unicornhat.show()
-		time.sleep(3.0)    
-		unicornhat.set_pixels(ripple00)
-		unicornhat.show()
-		time.sleep(0.05)
-		unicornhat.set_pixels(ripple01)
-		unicornhat.show()
-		time.sleep(0.05)
-		unicornhat.set_pixels(ripple02)
-		unicornhat.show()
-		time.sleep(0.05)
-		unicornhat.set_pixels(ripple03)
-		unicornhat.show()
-		time.sleep(0.05)
-		unicornhat.set_pixels(ripple04)
-		unicornhat.show()
-		time.sleep(0.05)
-		unicornhat.set_pixels(ripple05)
-		unicornhat.show()
-		time.sleep(0.05)
-		unicornhat.set_pixels(ripple06)
-		unicornhat.show()
-		time.sleep(0.05)
-		unicornhat.set_pixels(ripple07)
-		unicornhat.show()
-		time.sleep(0.05)
 
 def main():
-	while True:
-		pibow_candy_double_ripple_4()
+    """
+    This is the main function
+    """
+
+    print_unicornhat_header()
+
+    # Force white text after selecting random colored header
+    print("\033[1;37;40mPress Ctrl-C to stop the program.")
+
+    try:
+        while True:
+            pibow_candy_double_ripple_4()
+    except KeyboardInterrupt:
+        stop()
+
+
+
+def pibow_candy_double_ripple_4():
+    """
+    Ripples the Pibow Candy rainbow from the bottom left (next to the
+    USB ports) to the top right (The corner near the power port) as well
+    as from the bottom to the top (the side with the HDMI port).
+    """
+
+    rainbow = [
+        [R, O, Y, G, B, I, V, W],
+        [R, O, Y, G, B, I, V, W],
+        [R, O, Y, G, B, I, V, W],
+        [R, O, Y, G, B, I, V, W],
+        [R, O, Y, G, B, I, V, W],
+        [R, O, Y, G, B, I, V, W],
+        [R, O, Y, G, B, I, V, W],
+        [R, O, Y, G, B, I, V, W]
+    ]
+
+    ripple00 = [
+        [R2, O, Y, G, B, I, V, W],
+        [R2, O, Y, G, B, I, V, W],
+        [R2, O, Y, G, B, I, V, W],
+        [R2, O, Y, G, B, I, V, W],
+        [R2, O, Y, G, B, I, V, W],
+        [R2, O, Y, G, B, I, V, W],
+        [R2, O, Y, G, B, I, V, W],
+        [R2, O2, Y2, G2, B2, I2, V2, W2]
+    ]
+
+    ripple01 = [
+        [R, O2, Y, G, B, I, V, W],
+        [R, O2, Y, G, B, I, V, W],
+        [R, O2, Y, G, B, I, V, W],
+        [R, O2, Y, G, B, I, V, W],
+        [R, O2, Y, G, B, I, V, W],
+        [R, O2, Y, G, B, I, V, W],
+        [R2, O2, Y2, G2, B2, I2, V2, W2],
+        [R, O2, Y, G, B, I, V, W]
+    ]
+
+    ripple02 = [
+        [R, O, Y2, G, B, I, V, W],
+        [R, O, Y2, G, B, I, V, W],
+        [R, O, Y2, G, B, I, V, W],
+        [R, O, Y2, G, B, I, V, W],
+        [R, O, Y2, G, B, I, V, W],
+        [R2, O2, Y2, G2, B2, I2, V2, W2],
+        [R, O, Y2, G, B, I, V, W],
+        [R, O, Y2, G, B, I, V, W]
+    ]
+
+    ripple03 = [
+        [R, O, Y, G2, B, I, V, W],
+        [R, O, Y, G2, B, I, V, W],
+        [R, O, Y, G2, B, I, V, W],
+        [R, O, Y, G2, B, I, V, W],
+        [R2, O2, Y2, G2, B2, I2, V2, W2],
+        [R, O, Y, G2, B, I, V, W],
+        [R, O, Y, G2, B, I, V, W],
+        [R, O, Y, G2, B, I, V, W]
+    ]
+
+    ripple04 = [
+        [R, O, Y, G, B2, I, V, W],
+        [R, O, Y, G, B2, I, V, W],
+        [R, O, Y, G, B2, I, V, W],
+        [R2, O2, Y2, G2, B2, I2, V2, W2],
+        [R, O, Y, G, B2, I, V, W],
+        [R, O, Y, G, B2, I, V, W],
+        [R, O, Y, G, B2, I, V, W],
+        [R, O, Y, G, B2, I, V, W]
+    ]
+
+    ripple05 = [
+        [R, O, Y, G, B, I2, V, W],
+        [R, O, Y, G, B, I2, V, W],
+        [R, O, Y, G, B, I2, V, W],
+        [R2, O2, Y2, G2, B2, I2, V2, W2],
+        [R, O, Y, G, B, I2, V, W],
+        [R, O, Y, G, B, I2, V, W],
+        [R, O, Y, G, B, I2, V, W],
+        [R, O, Y, G, B, I2, V, W]
+    ]
+
+    ripple06 = [
+        [R, O, Y, G, B, I, V, W],
+        [R2, O2, Y2, G2, B2, I2, V2, W2],
+        [R, O, Y, G, B, I, V, W],
+        [R, O, Y, G, B, I, V, W],
+        [R, O, Y, G, B, I, V, W],
+        [R, O, Y, G, B, I, V, W],
+        [R, O, Y, G, B, I, V, W],
+        [R, O, Y, G, B, I, V, W]
+    ]
+
+    ripple07 = [
+        [R2, O2, Y2, G2, B2, I2, V2, W2],
+        [R, O, Y, G, B, I, V, W2],
+        [R, O, Y, G, B, I, V, W2],
+        [R, O, Y, G, B, I, V, W2],
+        [R, O, Y, G, B, I, V, W2],
+        [R, O, Y, G, B, I, V, W2],
+        [R, O, Y, G, B, I, V, W2],
+        [R, O, Y, G, B, I, V, W2]
+    ]
+
+    unicornhat.set_pixels(rainbow)
+    unicornhat.show()
+    sleep(3.0)
+    unicornhat.set_pixels(ripple00)
+    unicornhat.show()
+    sleep(0.05)
+    unicornhat.set_pixels(ripple01)
+    unicornhat.show()
+    sleep(0.05)
+    unicornhat.set_pixels(ripple02)
+    unicornhat.show()
+    sleep(0.05)
+    unicornhat.set_pixels(ripple03)
+    unicornhat.show()
+    sleep(0.05)
+    unicornhat.set_pixels(ripple04)
+    unicornhat.show()
+    sleep(0.05)
+    unicornhat.set_pixels(ripple05)
+    unicornhat.show()
+    sleep(0.05)
+    unicornhat.set_pixels(ripple06)
+    unicornhat.show()
+    sleep(0.05)
+    unicornhat.set_pixels(ripple07)
+    unicornhat.show()
+    sleep(0.05)
+
+
+def stop():
+    """
+    Print exit message and turn off the UnicornHAT
+    """
+    print("\nExiting program.")
+    unicornhat.off()
+
 
 if __name__ == '__main__':
     main()
