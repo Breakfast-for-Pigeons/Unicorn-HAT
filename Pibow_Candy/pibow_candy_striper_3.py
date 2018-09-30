49421bfeab0c2d6a310a83c401ccc8ee55c98bfc
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2018-09-30 13:35:28 -0500

    Update pibow_candy_striper_3.py
    Converted to Python 3

--- Pibow_Candy/pibow_candy_striper_3.py
+++ Pibow_Candy/pibow_candy_striper_3.py
@@ -1,68 +1,137 @@
-#!/usr/bin/env python
-################################################################
-#                    Pibow Candy Striper 3                     #
-################################################################
-# Description:                                                 #
-# This program lights up each column of the Unicorn pHAT with  #
-# its respective color.                                        #
-#                                                              #
-# Author: Paul Ryan                                            #
-#                                                              #
-################################################################
-import unicornhat, signal, time, random
+#!/usr/bin/python3
+"""
+Pibow Candy Striper 3
+
+This program lights up each column one at a time with the pastel colors
+of the Pimoroni Pibow Candy case. It lights them up from right to left
+(the side with the USB and ethernet ports) and from top (the
+side with the HDMI port) to the bottom.
+
+....................
+
+Functions:
+- pibow_candy_striper_3: Get the x and y coordinates and the
+  RGB integers for the x coordinates.
+- get_color: Extracts 3 individual integers from a tuple and returns
+  them.
+- stop: Print exit message and turn off the UnicornHAT
+
+....................
+
+Author: Paul Ryan
+
+This program was written on a Raspberry Pi using the Geany IDE.
+"""
+########################################################################
+#                          Import modules                              #
+########################################################################
+
+from time import sleep
+import unicornhat
+from print_unicornhat_header import print_unicornhat_header
+
+########################################################################
+#                           Initialize                                 #
+########################################################################
 
 unicornhat.set_layout(unicornhat.HAT)
 unicornhat.brightness(0.5)
 unicornhat.rotation(180)
 
-x_or_y_coordinates = [0, 1, 2, 3, 4, 5, 6, 7]
+X_OR_Y_COORDINATES = [0, 1, 2, 3, 4, 5, 6, 7]
 
-x0_color_tuple  = (255, 105, 97)
-x1_color_tuple  = (255, 179, 71)
-x2_color_tuple  = (253, 253, 150)
-x3_color_tuple  = (119, 190, 119)
-x4_color_tuple  = (119, 158, 203)
-x5_color_tuple  = (150, 111, 214)
-x6_color_tuple  = (203, 153, 201)
-x7_color_tuple  = (244, 154, 194)
+X0_COLOR_TUPLE = (255, 105, 97)
+X1_COLOR_TUPLE = (255, 179, 71)
+X2_COLOR_TUPLE = (253, 253, 150)
+X3_COLOR_TUPLE = (119, 190, 119)
+X4_COLOR_TUPLE = (119, 158, 203)
+X5_COLOR_TUPLE = (150, 111, 214)
+X6_COLOR_TUPLE = (203, 153, 201)
+X7_COLOR_TUPLE = (244, 154, 194)
 
-def get_color(x_color_tuple): 
-    return int(x_color_tuple[0]), int(x_color_tuple[1]), int(x_color_tuple[2]) 
-    
-def pibow_candy_striper_3():
-	
-	unicornhat.clear()
-	x_coordinate_list = x_or_y_coordinates[::-1]
-	y_coordinate_list = x_or_y_coordinates
-	
-	for x in x_coordinate_list:
-		#Get the RGB color for the x coordinate
-		if x == 0:
-			r, g, b, = get_color(x0_color_tuple)
-		if x == 1:
-			r, g, b, = get_color(x1_color_tuple)
-		if x == 2:
-			r, g, b, = get_color(x2_color_tuple)
-		if x == 3:
-			r, g, b, = get_color(x3_color_tuple)
-		if x == 4:
-			r, g, b, = get_color(x4_color_tuple)
-		if x == 5:
-			r, g, b, = get_color(x5_color_tuple)
-		if x == 6:
-			r, g, b, = get_color(x6_color_tuple)
-		if x == 7:
-			r, g, b, = get_color(x7_color_tuple)						
-		# Light up selected x column
-		for y in y_coordinate_list:
-			unicornhat.set_pixel(x, y, r, g, b)
-			unicornhat.show()
-			time.sleep(0.05)
-	time.sleep(1.0)
+########################################################################
+#                            Functions                                 #
+########################################################################
+
 
 def main():
-	while True:
-		pibow_candy_striper_3()
-		
+    """
+    This is the main function
+    """
+
+    print_unicornhat_header()
+
+    # Force white text after selecting random colored header
+    print("\033[1;37;40mPress Ctrl-C to stop the program.")
+
+    try:
+        while True:
+            pibow_candy_striper_3()
+    except KeyboardInterrupt:
+        stop()
+
+
+def pibow_candy_striper_3():
+    """
+    Gets the x and y coordinates and the RGB integers for the
+    x coordinates.
+    """
+
+    unicornhat.clear()
+    x_coordinate_list = X_OR_Y_COORDINATES[::-1]
+    y_coordinate_list = X_OR_Y_COORDINATES
+
+    for x_coordinate in x_coordinate_list:
+        #Get the RGB color for the x_coordinate coordinate
+        if x_coordinate == 0:
+            red, green, blue, = get_color(X0_COLOR_TUPLE)
+        if x_coordinate == 1:
+            red, green, blue, = get_color(X1_COLOR_TUPLE)
+        if x_coordinate == 2:
+            red, green, blue, = get_color(X2_COLOR_TUPLE)
+        if x_coordinate == 3:
+            red, green, blue, = get_color(X3_COLOR_TUPLE)
+        if x_coordinate == 4:
+            red, green, blue, = get_color(X4_COLOR_TUPLE)
+        if x_coordinate == 5:
+            red, green, blue, = get_color(X5_COLOR_TUPLE)
+        if x_coordinate == 6:
+            red, green, blue, = get_color(X6_COLOR_TUPLE)
+        if x_coordinate == 7:
+            red, green, blue, = get_color(X7_COLOR_TUPLE)
+        # Light up selected x_coordinate column
+        for y_coordinate in y_coordinate_list:
+            unicornhat.set_pixel(x_coordinate, y_coordinate,
+                                 red, green, blue)
+            unicornhat.show()
+            sleep(0.05)
+    sleep(1.0)
+
+
+def get_color(x_color_tuple):
+    """
+    Extracts 3 individual integers from a tuple and returns them.
+
+    Arguments:
+        x_coordinate_color_tuple: a tuple of red, green, and blue
+        integers
+
+    Returns:
+        3 integers representing red, green, and blue respectively
+    """
+
+    return int(x_color_tuple[0]), \
+           int(x_color_tuple[1]), \
+           int(x_color_tuple[2])
+
+
+def stop():
+    """
+    Print exit message and turn off the UnicornHAT
+    """
+    print("\nEx_coordinateiting program.")
+    unicornhat.off()
+
+
 if __name__ == '__main__':
     main()
